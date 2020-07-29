750f3a37c5ab62e814dd89860d187dde756d7fe5
Julian Tekook <julian.tekook@toefi.de>
2020-07-29 14:35:22 +0200

    Add constructors

--- Tekook.CliConfigurator/EnvironmentAttribute.cs
+++ Tekook.CliConfigurator/EnvironmentAttribute.cs
@@ -5,11 +5,28 @@ namespace Tekook.CliConfigurator
     /// <summary>
     /// Attribute for Properties to map an env value.
     /// </summary>
+
     public class EnvironmentAttribute : Attribute
     {
         /// <summary>
         /// Name of the env variable.
         /// </summary>
         public string Name;
+
+        /// <summary>
+        /// Creates a new Instance.
+        /// </summary>
+        public EnvironmentAttribute()
+        {
+        }
+
+        /// <summary>
+        /// Creates a new Instance.
+        /// </summary>
+        /// <param name="name">Name of the env variable.</param>
+        public EnvironmentAttribute(string name) : this()
+        {
+            this.Name = name;
+        }
     }
 }
