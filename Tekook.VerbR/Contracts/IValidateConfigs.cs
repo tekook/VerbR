94467e73171920ac9e5288a70474d8f455daca5d
Julian Tekook <Julian@Tekook.de>
2021-01-03 15:30:23 +0100

    add missing doc

--- Tekook.VerbR/Contracts/IValidateConfigs.cs
+++ Tekook.VerbR/Contracts/IValidateConfigs.cs
@@ -10,12 +10,14 @@ namespace Tekook.VerbR.Contracts
         /// <summary>
         /// Determinates if the config is valid.
         /// </summary>
+        /// <param name="config">The configuration to validate.</param>
         /// <returns>true if the config is valid.</returns>
         bool IsValid(T config);
 
         /// <summary>
         /// Determinates if the config is valid.
         /// </summary>
+        /// <param name="config">The configruation to validate.</param>
         /// <param name="errors">Output of <see cref="IValidationError"/> which occurred during validation.</param>
         /// <returns>true if the config is valid.</returns>
         bool IsValid(T config, out IEnumerable<IValidationError> errors);
